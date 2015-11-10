dfea9b7afe6d748165cf63e2d05dca1fe544c8ff
Cheng Zhang <zhangchengx@gmail.com>
2015-11-10 16:47:51 -0500

    Add JS Announcement

---
+++ something.js
@@ -0,0 +1,4 @@
+var oDiv = document.createElement('div');
+oDiv.innerHTML = '<h3>Blog 已经转移到 GitHub Pages，暂不更新。</h3>';
+var oHeader = document.getElementById("header");
+document.getElementById("main").insertBefore(oDiv, oHeader);
